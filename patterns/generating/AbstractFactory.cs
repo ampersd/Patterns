834ea2bdec6750a56cfe4cb64098c26fdaba8c62
ampersd <tony1000@ya.ru>
2015-06-06 00:40:15 +0500

    some examples
    abstract factory and bridge examples

--- patterns/generating/AbstractFactory.cs
+++ patterns/generating/AbstractFactory.cs
@@ -1,6 +1,62 @@
 ﻿namespace patterns.generating
 {
-    class AbstractFactory
+    class AbstractFactoryUse
     {
+        AbstractFactoryUse()
+        {
+            GuiFactory guiFactory = new MotifFactory(); // Можем подставить конкретную реализацию интерфейса в этой строчке
+            ScrollBar sb = guiFactory.CreateScrollBar();
+        }
+    }
+
+    /// <summary>
+    /// Абстрактная фабрика
+    /// Определяет общий интерфейс для создания объектов
+    /// В примере создается ScrollBar, но на самом деле должны быть методы для создания других объектов
+    /// Button, Grid и пр.
+    /// Особенностью паттерна является наличие Create... в названии операций
+    /// </summary>
+    abstract class GuiFactory
+    {
+        public abstract ScrollBar CreateScrollBar();
+    }
+
+    /// <summary>
+    /// Реализация происходит в подклассах
+    /// </summary>
+    class MotifFactory : GuiFactory
+    {
+        public override ScrollBar CreateScrollBar()
+        {
+            return new MotifScrollBar();
+        }
+    }
+
+    class MacFactory : GuiFactory
+    {
+        public override ScrollBar CreateScrollBar()
+        {
+            return new MacScrollBar();
+        }
+    }
+
+    /// <summary>
+    /// Глиф - это любой графический элемент, аналог класса object в c#
+    /// </summary>
+    class Glyph
+    {
+    }
+    abstract class ScrollBar : Glyph
+    {
+    }
+
+    class MotifScrollBar : ScrollBar
+    {
+        
+    }
+
+    class MacScrollBar : ScrollBar
+    {
+        
     }
 }
--- patterns/structural/Bridge.cs
+++ patterns/structural/Bridge.cs
@@ -9,4 +9,38 @@ namespace patterns.behavioral
     class Bridge
     {
     }
+    /// <summary>
+    /// Зависимость от оконной системы содержится в классе реализации
+    /// </summary>
+    abstract class Window
+    {
+        private WindowImp _imp = new MacWindowImp();
+        // задавать нужную реализацию можно через абстрактную фабрику, например 
+        // WindowSystemFactory windowSystemFactory = new MacSystemFactory(); - где-то во время инициализации фабрики
+        // _imp = windowSystemFactory.CreateWindowImp(); - в этом месте
+
+        public void Raise()
+        {
+            _imp.DeviceRaise();
+        }
+
+        public void Rect()
+        {
+            _imp.DeviceRect();
+        }
+    }
+    
+    abstract class WindowImp
+    {
+        public abstract void DeviceRaise();
+        public abstract void DeviceRect();
+    }
+    /// <summary>
+    /// Конкретная реализация оконной системы
+    /// </summary>
+    class MacWindowImp : WindowImp
+    {
+        public override void DeviceRaise(){}
+        public override void DeviceRect(){}
+    }
 }
